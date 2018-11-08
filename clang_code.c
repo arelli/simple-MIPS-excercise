5c60d4d330046b1ea68ccbdf1efd7e307bf96bb9
iamoziroz <nickpap556@gmail.com>
2018-11-08 14:08:43 +0200

    Update clang_code.c
    i changed the register names to conventional MIPS ones, BUT there is a segmentation fault when i run the multiplier()

--- clang_code.c
+++ clang_code.c
@@ -13,129 +13,131 @@ int k0,k1,gp,sp,fp,ra;
 int* givenArray;
 int* retArray;
 
-int func1(int R1){
+int printTriangle(int a0){
 
     int z=0, i=0, j=0;
 
-  	R2=z;
-  	R3=i;
-  	R14=j;
+  	t0=z;
+  	t1=i;
+  	t9=j;//THIS was problematic, conflict with other registers after first loop
 
     before_while3:
-    if (R3>=R1) goto after_while3;
+    if (t1>=a0) goto after_while3;
         before_while4:
-        if (R14>R3)goto after_while4;
-            printf("%4d", R14+1);
-            R14=R14+1;
-            R2=R2+1;
+        if (t9>t1)goto after_while4;
+            printf("%4d", t9+1);
+            t9=t9+1;
+            t0=t0+1;
         goto before_while4;
         after_while4:
         printf("\n ");
-        R3=R3+1;
-        R14=0;
+        t1=t1+1;
+        t9=0;
     goto before_while3;
     after_while3:
     printf("\n ");
-    return R2;
+    v0=t0;//t0 is not preserved after the return of the function
+    return v0;
 }
 
-int func2(int R1){
-    int R2 = 0;
-    if (R1&1 == 1){
+int checkNumber(int a0){
+    int v0 = 0;
+    if (a0&1 == 1){
         return 1;
     }
-    return R2;
+    return v0;
 }
 
-int func3(int R1, int R2){
+int multiplier(int a0, int a1){
 
-    int R3=0;//i
+    int t0=0;//i
     printf("\n");
     before_while5:
-    if(R3>=5) goto after_while5;//while(i<5)
-        printf(" -Give integer n.%d: ", R3+1);
-        scanf("%d", &(*((int*)R1+R3)));
-        *((int*)R2+R3) = *((int*)R1+R3)+ *((int*)R1+R3)+ *((int*)R1+R3)+ *((int*)R1+R3)+ *((int*)R1+R3);
-        R3=R3+1;
+    if(t0>=5) goto after_while5;//while(i<5)
+        printf(" -Give integer n.%d: ", t0+1);
+        scanf("%d", &(*((int*)a0+t0))); //TODO: convert one-line addition to multiline
+        *((int*)a1+t0) = *((int*)a0+t0)+ *((int*)a0+t0)+ *((int*)a0+t0)+ *((int*)a0+t0)+ *((int*)a0+t0);
+        t0=t0+1;
     goto before_while5;
     after_while5:
         printf("\n");
-        return R2;
+	v0=a1; //a1 register is NOT preserved on subprograms(deleted after the function returns)
+        return v0; //by convention, the return value is passed through v0 and v1
 }
 
 int main()
 {
-    givenArray  = (int*)malloc(5*sizeof(int));
+    givenArray  = (int*)malloc(5*sizeof(int));//TODO: allocate space in .data section
 
-    retArray = (int*)malloc(5*sizeof(int));
+    retArray = (int*)malloc(5*sizeof(int)); //TODO: allocate space in .data section
 
-    int* mainArray = (int*)malloc(5*sizeof(int));
+    int* mainArray = (int*)malloc(5*sizeof(int)); //TODO: allocate space in .data section
 
     int ret1, ret2;
     int n1,n2;
     int k = 0;
     int choice;
 
-    R7 = (int)givenArray;
-    R8 = (int)retArray;
-    R9 = (int)mainArray;
+    t6 = (int)givenArray;
+    t7 = (int)retArray;
+    t8 = (int)mainArray;
 
-    R1 = ret1;
-    R2 = ret2;
-    R3 = k;
-    R4 = choice;
-    R5 = n1;
-    R6 = n2;
+    t0 = ret1;
+    t1 = ret2;
+    t2 = k;
+    t3 = choice;
+    t4 = n1;
+    t5 = n2;
 
     before_while:
     if(!1) goto after_while;//while(1){ // if(0)
         before_choice:
         printf("\n ~~~~~HRY201-Lab3~~~~~~~~");
         printf("\n --------Menu------------");
-        printf("\n 1. Function n.1---------");
-        printf("\n 2. Function n.2---------");
-        printf("\n 3. Function n.3---------");
+        printf("\n 1. Print Triangle-------");
+        printf("\n 2. Check Number---------");
+        printf("\n 3. Array Multiplier-----");
         printf("\n 4. Exit-----------------");
         printf("\n ~~~~~~~~~~~~~~~~~~~~~~~~");
         printf("\n\n  Give your choice: ");
-        scanf("%d", &R4);
+        scanf("%d", &t3);
 		//switch(choice){
-        if (R4 == 1){//case 1:
+        if (t3 == 1){//case 1:
             printf("\n Give the number of rows you want for your Triangle ");
             printf(" \n        (if you want to exit give -1 or 0):        ");
-            scanf("%d", &R5);
+            scanf("%d", &t4);
             printf("\n ");
-            if (R5==-1 || R5==0){
+            if (t4==-1 || t4==0){
                 printf("\n  Program Finished!");
                 return 0;
             }
-            R1 = func1(R5);
-            printf("\n  The number of printed values is: %d\n", R1);
+            t0 = printTriangle(t4);
+            printf("\n  The number of printed values is: %d\n", t0);
         }
-        if (R4 == 2){//case 2:
+        if (t3 == 2){//case 2:
             printf("\n Give the number you want to check: ");
-            scanf("%d", &R6);
-            R2 = func2(R6);
-            if (R2==0)
+            scanf("%d", &t5);
+            t1 = checkNumber(t5);
+            if (t1==0)
                 printf("\n  --> The number is EVEN!\n");
-            if (R2==1)
+            if (t1==1)
                 printf("\n  --> The number is ODD!\n");
         }
-        if (R4==3){//case 3:
-            R9 = func3(R7, R8); // or R9 = func3((int*)R7, (int*)R8);
+        if (t3==3){//case 3:
+            t8 = multiplier(t6, t7); // or t8 = multiplier((int*)t6, (int*)t7);
             before_while2:
-            if(R3>=5) goto after_while2;
-                printf("\n The number %d multiplied by five is equal to:  %d", *((int*)R7+R3),*((int*)R9+R3));
-                R3++;
+            if(t2>=5) goto after_while2;
+                printf("\n The number %d multiplied by five is equal to:  %d", *((int*)t6+t2),*((int*)t8+t2));
+                t2++;
             goto before_while2;
             after_while2:
             printf("\n");
         }
-        if (R4==4){//case 4:
+        if (t3==4){//case 4:
             printf("\n Programm Finished!\n");
             exit(0);
         }
-        if ( R4!=4 && R4!=2 && R4!=3 && R4!=1){ //|| choice-(float)choice!=0){
+        if ( t3!=4 && t3!=2 && t3!=3 && t3!=1){ //|| choice-(float)choice!=0){
             printf("\n Choice not available! Try again!\n");
             goto before_choice;
         }
