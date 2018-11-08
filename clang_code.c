ac951f46b1d093bd0a1acd0cdbb687f58a8dcd6c
iamoziroz <nickpap556@gmail.com>
2018-11-08 13:08:26 +0200

    Update clang_code.c
    added the original-name pointers

--- clang_code.c
+++ clang_code.c
@@ -7,6 +7,9 @@
 int R0=0, R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13,R14,R15,R16,R17,R18,R19,R20,R21;
 int R22,R23,R24,R25,R26,R27,R28,R29,R30,R31;
 
+int zero = 0, at,v0,v1,a0,a1,a2,a3,t0,t1,t2,t3,t4,t5,t6,t7,s0,s1,s2,s3,s4,s5,s6,s7,t8,t9;
+int k0,k1,gp,sp,fp,ra;
+
 int* givenArray;
 int* retArray;
 
